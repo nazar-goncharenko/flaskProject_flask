61aeafddee46127361e207e09c937be273f8250e
techsky <55064294+nazar-goncharenko@users.noreply.github.com>
2020-11-01 21:45:34 +0200

    commit3

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/api/v1/hello-world-<var>')
 def hello_world(var):
-    return 'Hello World' + str(var)
+    return 'Hello World ' + str(var)
 
 
 if __name__ == '__main__':

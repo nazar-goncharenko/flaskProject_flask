ba62d401d76309a79b6907e6dd7d0872b998545d
techsky <55064294+nazar-goncharenko@users.noreply.github.com>
2020-11-02 01:09:12 +0200

    LAST

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
+from wsgiref.simple_server import make_server
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/api/v1/hello-world-<var>')
-def hello_world(var):
-    return 'Hello World ' + str(var)
+@app.route('/api/v1/hello-world-5')
+def hello_world():
+    return 'Hello World 5'
 
 
-if __name__ == '__main__':
-    app.run()
+with make_server('', 5000, app) as server:
+    print("Something http://127.0.0.1:5000")
+
+    server.serve_forever()

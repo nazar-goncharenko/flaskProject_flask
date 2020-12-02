3f10d51db9ac099098c21bd2698621c9027b5c14
Sviatoslav <55582196+fayonn@users.noreply.github.com>
2020-12-02 06:34:39 +0200

    laba_6_pp_fayon

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -9,12 +9,12 @@ from sqlalchemy.orm import sessionmaker
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'secret key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/pp_6'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{user}:{password}@{server}/{database}'.format(
+    user='root', password='root', server='localhost', database='pp_6')
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 manager = Manager(app)
 db = SQLAlchemy(app)
-
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
@@ -23,9 +23,3 @@ engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 SessionFactory = sessionmaker(bind=engine)
 
 BaseModel = declarative_base()
-
-db.create_all()
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
--- test.py
+++ test.py
@@ -1,4 +1,21 @@
 from models import *
 
+# user1 = User(userName="testname", password="123", email="test@gmail.com", firstName="saf", lastName="sdaf", phone="fasd", userStatus=23)
+# photo1 = Photo(photoUrl="someUrl")
+# article1 = Article(name="Some Article")
+# photos1 = Photos(article=article1, photo=photo1)
+
+user1 = User(userName="testname", password="123", email="test@gmail.com", firstName="saf", lastName="sdaf", phone="fasd", userStatus=23)
+user2 = User(userName="2", password="2", email="2", firstName="2", lastName="2", phone="2", userStatus=23)
+user3 = User(userName="3", password="3", email="3", firstName="3", lastName="3", phone="3", userStatus=23)
+
 db.create_all()
 
+db.session.add(user1)
+db.session.add(user3)
+db.session.add(user2)
+# db.session.add(photo1)
+# db.session.add(article1)
+# db.session.add(photos1)
+
+db.session.commit()
